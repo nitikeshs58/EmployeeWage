47a1bc82aeeb1c4bee2e00d0407573ffee678efd
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-18 19:10:52 +0530

    Added: checked employee is present or not using ((RANDOM)) .

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
+attendance=$((RANDOM%2))
+if [[ attendance -eq 0 ]]
+then
+	echo "Emp Present"
+else
+	echo "Emp Absent"
+fi
