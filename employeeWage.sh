b94b2054ec61071dede6c2ce18ba07f73c75ff91
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-18 23:28:32 +0530

    Added: calculated wages for 20 working day per month.

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-wagePerHour=20
-fullTime=8
-partTime=4
-attendance=$((RANDOM%3))
+WAGE_PER_HOUR=20
+FULL_TIME=8
+PART_TIME=4
+sal=0
+for (( counter=0; counter<20; counter++ ))
+do
+	attendance=$((RANDOM%3))
 
-case $attendance in
-	0)
-		echo "Employee Present"
-		sal=$((wagePerHour*fullTime))
-		;;
-	1)
-		echo "PartTime emp"
-		sal=$((wagePerHour*partTime))
-		;;
-	2)
+	case $attendance in
+		0)
+			echo "Employee Present"
+			sal=$((sal+WAGE_PER_HOUR*FULL_TIME))
+			;;
+		1)
+			echo "PartTime emp"
+			sal=$((sal+WAGE_PER_HOUR*PART_TIME))
+			;;
+		2)
 		echo "Employee Absent"
 		;;
-esac
+	esac
+done
+echo $sal
+
