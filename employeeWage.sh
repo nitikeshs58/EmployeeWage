88cde52fb3e53086f5f179e07b6d4e4b166f1329
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-18 20:22:59 +0530

    Added: Salary of fulltime daily Employee wage.

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-
+wagePerHour=20
+fullTime=8
 attendance=$((RANDOM%2))
 if [[ attendance -eq 0 ]]
 then
-	echo "Emp Present"
+	echo "Employee Present"
+	sal=$((wagePerHour*fullTime))
 else
 	echo "Emp Absent"
 fi
