f26d8dc6ff9553bb8251395f342cd974060ff1cc
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-18 20:48:11 +0530

    Added: PartTime emp wage.

--- employeeWage.sh
+++ employeeWage.sh
@@ -2,11 +2,16 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 wagePerHour=20
 fullTime=8
-attendance=$((RANDOM%2))
+partTime=4
+attendance=$((RANDOM%3))
 if [[ attendance -eq 0 ]]
 then
 	echo "Employee Present"
 	sal=$((wagePerHour*fullTime))
+elif [[ attendance -eq 1 ]]
+then
+	echo "Parttime emp :"
+	sal=$((wagePerHour*partTime))
 else
 	echo "Emp Absent"
 fi
