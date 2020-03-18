ca71d60822a38135b920c1396481582c64fb0edb
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-19 00:28:32 +0530

    Added: Calculated total working hours or wages using condition of a month.

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,22 +1,41 @@
 #!/bin/bash -x
+
 echo "Welcome to Employee Wage Computation Program on Master Branch"
+
 WAGE_PER_HOUR=20
 FULL_TIME=8
 PART_TIME=4
 sal=0
-for (( counter=0; counter<20; counter++ ))
+hour=0
+day=0
+
+while [[ $day -lt 20 ]]
 do
 	attendance=$((RANDOM%3))
 
 	case $attendance in
 		0)
-			echo "Employee Present"
-			sal=$((sal+WAGE_PER_HOUR*FULL_TIME))
-			;;
+		echo "Employee Present"
+		sal=$((sal+WAGE_PER_HOUR*FULL_TIME))
+		hour=$((hour+FULL_TIME))
+		day=$((day+1))
+		if [[ $hour -eq 100 ]]
+		then
+			break
+		fi
+		;;
+
 		1)
-			echo "PartTime emp"
-			sal=$((sal+WAGE_PER_HOUR*PART_TIME))
-			;;
+		echo "PartTime emp"
+		sal=$((sal+WAGE_PER_HOUR*PART_TIME))
+		hour=$((hour+PART_TIME))
+		day=$((day+1))
+		if [[ $hour -eq 100 ]]
+		then
+			break
+		fi
+		;;
+
 		2)
 		echo "Employee Absent"
 		;;
