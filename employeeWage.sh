c2a0900220c9d28626239fcd9b7071f1f8a02443
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-19 13:34:33 +0530

    Added: get work hours using function.

--- employeeWage.sh
+++ employeeWage.sh
@@ -5,41 +5,41 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 WAGE_PER_HOUR=20
 FULL_TIME=8
 PART_TIME=4
+ABSENT=0
+EMP_IS_FULLTIME=0
+EMP_IS_PARTTIME=1
+EMP_IS_ABSENT=2
+
 sal=0
 hour=0
 day=0
 
-while [[ $day -lt 20 ]]
-do
-	attendance=$((RANDOM%3))
+function getWorkingHours()
+{
+	local attendance=$1
 
 	case $attendance in
-		0)
-		echo "Employee Present"
-		sal=$((sal+WAGE_PER_HOUR*FULL_TIME))
-		hour=$((hour+FULL_TIME))
-		day=$((day+1))
-		if [[ $hour -eq 100 ]]
-		then
-			break
-		fi
-		;;
+		$EMP_IS_FULLTIME)
+			echo "$FULL_TIME"
+			;;
 
-		1)
-		echo "PartTime emp"
-		sal=$((sal+WAGE_PER_HOUR*PART_TIME))
-		hour=$((hour+PART_TIME))
-		day=$((day+1))
-		if [[ $hour -eq 100 ]]
-		then
-			break
-		fi
-		;;
+		$EMP_IS_PARTTIME)
+			echo "$PART_TIME"
+			;;
 
-		2)
-		echo "Employee Absent"
-		;;
+		$EMP_IS_ABSENT)
+			echo "$ABSENT"
+			;;
 	esac
+}
+while [[ $day -ne 20 && $hour -ne 100 ]]
+do
+	attendance=$((RANDOM%3))
+	hourPerDay=$( getWorkingHours $attendance )
+	hour=$(($hour+$hourPerDay))
+	((day++))
+	sal=$((sal+hourPerDay*WAGE_PER_HOUR));
 done
-echo $sal
 
+echo "Monthly Sal:       " $sal
+echo "Total Working Hours: "$hour
